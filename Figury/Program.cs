9e42afbcdb29a1bde66c053fcd655aea97270e85
Divix <divix55@icloud.com>
2016-03-20 11:47:20 +0100

    commit 1

---
+++ Figury/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wlasciwosci obiekt = new Wlasciwosci();
+            Console.WriteLine("Witaj!");
+            Console.WriteLine("");
+            Console.WriteLine("Jestem programem liczącym właściwości figury prawidłowej, której parametry zaraz podasz!");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("Jaka jest długość krawędzi?");
+            obiekt.krawedz = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ile ma kątów?");
+            obiekt.katy = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("Pole=" + obiekt.pole(obiekt.krawedz, obiekt.katy));
+            Console.WriteLine("Obwód=" + obiekt.obwod(obiekt.krawedz, obiekt.katy));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Figury/Wlasciwosci.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figury
+{
+    class Wlasciwosci
+    {
+        private double _krawedz;
+        private int _katy;
+
+        public double krawedz
+        {
+            get
+            {
+                if (_krawedz > 0)
+                    return _krawedz;
+                else
+                    return 0;
+            }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Dlugosc krawedzi musi byc wieksza od 0!");
+                else
+                    _krawedz = value;
+            }
+        }
+        public int katy
+        {
+            get
+            {
+                if (_katy > 2)
+                    return _katy;
+                else
+                    return 0;
+            }
+            set
+            {
+                if (value < 2)
+                {
+                    Console.WriteLine("Ilość kątów musi być większa lub równa 3!");
+                }
+                else
+                    _katy = value;
+            }
+        }
+
+        public double pole(double krawedz, int kat)
+        {
+            double r = (krawedz / 2) * (1 / Math.Tan(Math.PI / kat));
+            return (krawedz * kat * r) / 2;
+        }
+
+        public double obwod(double krawedz, int kat)
+        {
+            return krawedz * kat;
+        }
+    }
+}
